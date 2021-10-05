84ee12cf5a979fc1efd4f099466b79f46d93bc19
Jonas S. Bezerra <jonassantosbezerra@outlook.com>
2021-10-05 14:41:44 -0300

    refactor

--- lib/todo.ex
+++ lib/todo.ex
@@ -1,14 +1,21 @@
 defmodule Todo do
+  defstruct auto_id: 1, entries: %{}
 
   def new() do
-    Dictionary.new()
+    %Todo{}
   end
 
-  def add_entry(todo, date, title) do
-    Dictionary.add(todo, date, title)
+  def add_entry(todo, entry) do
+    entry = Map.put(entry, :id, todo.auto_id)
+
+    new_entries = Map.put(todo.entries, todo.auto_id, entry)
+
+    %Todo{todo | entries: new_entries, auto_id: todo.auto_id + 1}
   end
 
   def entries(todo, date) do
-    Map.get(todo, date)
+    todo.entries
+    |> Stream.filter(fn {_, entry} -> entry.date == date end)
+    |> Enum.map(fn {_, entry} -> entry end)
   end
 end
