2fb66ca757b706d5dcdbf9f788d95e6acbe28eb5
Jonas S. Bezerra <jonassantosbezerra@outlook.com>
2021-10-05 16:19:50 -0300

    feat: define update entry functions

--- lib/todo.ex
+++ lib/todo.ex
@@ -18,4 +18,21 @@ defmodule Todo do
     |> Stream.filter(fn {_, entry} -> entry.date == date end)
     |> Enum.map(fn {_, entry} -> entry end)
   end
+
+  def update_entry(todo, %{} = new_entry) do
+    update_entry(todo, new_entry.id, fn _ -> new_entry end)
+  end
+
+  def update_entry(todo, entry_id, updater_lambda) do
+    case Map.fetch(todo.entries, entry_id) do
+      :error ->
+        todo
+
+      {:ok, old_entry} ->
+        old_entry_id = old_entry.id
+        new_entry = %{id: ^old_entry_id} = updater_lambda.(old_entry)
+        new_entries = Map.put(todo.entries, new_entry.id, new_entry)
+        %Todo{todo | entries: new_entries}
+    end
+  end
 end
