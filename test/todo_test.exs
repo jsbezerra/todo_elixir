9b02161c726834cf1bd732d1d2a8d2c8dea1e075
Jonas S. Bezerra <jonassantosbezerra@outlook.com>
2021-10-11 19:39:37 -0300

    test: new todo server and add entries

--- test/todo_test.exs
+++ test/todo_test.exs
@@ -2,7 +2,19 @@ defmodule TodoTest do
   use ExUnit.Case
   doctest Todo
 
-  test "greets the world" do
-    assert Todo.hello() == :world
+  test "newly created todo has no entries" do
+    server = TodoServer.start()
+    entries = TodoServer.entries(server)
+    assert map_size(entries) == 0
+  end
+
+  test "new entries are persisted" do
+    server = TodoServer.start()
+    TodoServer.add_entry(server, %{date: ~D[2018-12-19], title: "Dentist"})
+    TodoServer.entries(server)
+    assert map_size(TodoServer.entries(server)) == 1
+
+    TodoServer.add_entry(server, %{date: ~D[2018-12-20], title: "Office"})
+    assert map_size(TodoServer.entries(server)) == 2
   end
 end
