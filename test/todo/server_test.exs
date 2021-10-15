5f1709d1b4926ba9b449b8687e345af0b6ccf1a8
Jonas S. Bezerra <jonassantosbezerra@outlook.com>
2021-10-15 11:36:40 -0300

    test: improve assertions in server tests

--- test/todo/server_test.exs
+++ test/todo/server_test.exs
@@ -4,8 +4,7 @@ defmodule Todo.Server.Test do
 
   test "newly created todo has no entries" do
     {:ok, pid} = Todo.Server.start()
-    entries = Todo.Server.entries(pid)
-    assert length(entries) === 0
+    assert length(Todo.Server.entries(pid)) === 0
   end
 
   test "new entries are persisted" do
@@ -20,44 +19,42 @@ defmodule Todo.Server.Test do
 
   test "retrieve entries by date" do
     {:ok, pid} = Todo.Server.start()
+
     Todo.Server.add_entry(pid, %{date: ~D[2018-12-19], title: "Dentist"})
     Todo.Server.add_entry(pid, %{date: ~D[2018-12-20], title: "Office"})
-    entries = [head | _] = Todo.Server.entries(pid, ~D[2018-12-20])
-    assert length(entries) === 1
-    assert head.title === "Office"
-    assert head.id === 2
-    assert head.date === ~D[2018-12-20]
+
+    assert [%{id: 2, date: ~D[2018-12-20], title: "Office"}] = Todo.Server.entries(pid, ~D[2018-12-20])
   end
 
   test "delete entries" do
     {:ok, pid} = Todo.Server.start()
     Todo.Server.add_entry(pid, %{date: ~D[2018-12-19], title: "Dentist"})
     Todo.Server.add_entry(pid, %{date: ~D[2018-12-20], title: "Office"})
-    assert length(Todo.Server.entries(pid)) === 2
+
+    assert [
+             %{id: 1, date: ~D[2018-12-19], title: "Dentist"},
+             %{id: 2, date: ~D[2018-12-20], title: "Office"}
+           ] = Todo.Server.entries(pid)
 
     Todo.Server.delete_entry(pid, 2)
-    entries = [head | _] = Todo.Server.entries(pid)
-    assert length(entries) === 1
-    assert head.title === "Dentist"
-    assert head.id === 1
-    assert head.date === ~D[2018-12-19]
+
+    assert [%{id: 1, date: ~D[2018-12-19], title: "Dentist"}] = Todo.Server.entries(pid)
   end
 
   test "update entry" do
     {:ok, pid} = Todo.Server.start()
     Todo.Server.add_entry(pid, %{date: ~D[2018-12-19], title: "Dentist"})
     Todo.Server.add_entry(pid, %{date: ~D[2018-12-20], title: "Office"})
-    assert length(Todo.Server.entries(pid)) === 2
-    [head | _] = Todo.Server.entries(pid)
-    assert head.title === "Dentist"
-    assert head.id === 1
-    assert head.date === ~D[2018-12-19]
+
+    assert [
+             %{id: 1, date: ~D[2018-12-19], title: "Dentist"},
+             %{id: 2, date: ~D[2018-12-20], title: "Office"}
+           ] = Todo.Server.entries(pid)
 
     new_entry = %{id: 1, date: ~D[2018-12-21], title: "Not Dentist Anymore"}
     Todo.Server.update_entry(pid, new_entry)
-    [head | _] = Todo.Server.entries(pid)
-    assert head.title === "Not Dentist Anymore"
-    assert head.id === 1
-    assert head.date === ~D[2018-12-21]
+
+    assert [^new_entry, %{id: 2, date: ~D[2018-12-20], title: "Office"}] =
+             Todo.Server.entries(pid)
   end
 end
