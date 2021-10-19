39018546c2f36cf7292e9b34eaa894ef927cbbca
Jonas S. Bezerra <jonassantosbezerra@outlook.com>
2021-10-19 00:30:43 -0300

    chore: format mix

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Todo.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.24", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
     ]
   end
 end
