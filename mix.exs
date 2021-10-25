036f68a40119ae28eae61aa3191cdf316311ea42
Jonas S. Bezerra <jonassantosbezerra@outlook.com>
2021-10-25 09:42:01 -0300

    chore: add poolboy dependency

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule Todo.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false},
+      {:poolboy, "~> 1.5"}
     ]
   end
 end
